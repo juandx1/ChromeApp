8ee09d8399c51c3d9d29a0ee8675ae434a001c0b
ElMarmos <alejandromarmolejos@hotmail.com>
2015-03-21 21:59:35 -0500

    Ajax editado

--- Aplicacion/ajax.js
+++ Aplicacion/ajax.js
@@ -17,3 +17,27 @@ function buscarPartida(summoner) {
 	});
 
 };
+
+function obtenerInformacionPersonal(){
+	$.ajax({
+		url: 'https://lan.api.pvp.net/api/lol/lan/v1.4/summoner/by-name/vladlord snow?api_key=33b1e8e7-58e3-4e15-a8bb-157f50a879a5',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(data) {
+		var theValue = textarea.value;
+        if (!theValue) {
+          message('Error: No value specified');
+          return;
+        }
+        chrome.storage.sync.set({'value': theValue}, function() {
+          message('Settings saved');
+        });
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+}
